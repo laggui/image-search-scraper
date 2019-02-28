01f546d25ff39a13158a4dc97e0a4fb875d67e0e
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2019-02-27 20:12:56 -0500

    Added download_all_images to download a list of image links accross multiple threads

--- webscraper/download_images.py
+++ webscraper/download_images.py
@@ -1,13 +1,18 @@
 import os
 import requests
+import concurrent.futures
+
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from requests.exceptions import HTTPError, ConnectionError
 
-def download_image(url, dest_dir, filename=None):
+from webscraper import SupportedSearchClients
+
+def download_image(dest_dir: str, image_dict: dict):
     '''
     Scrape images from website url and save them to destination directory
     '''
-    if filename is None:
-        filename = url.split('/')[-1]
+    filename = image_dict['file']
+    url = image_dict['url']
     
     headers = {'User-Agent': 'Mozilla/5.0'} # add header info for sites trying to block scraping
 
@@ -32,4 +37,24 @@ def download_image(url, dest_dir, filename=None):
         local_file.write(r.content)
         local_file.close()
     #f.close()
-    return img, "success"
+    return img, "Success"
+
+def download_all_images(query: str, download_dir: str, links: list, query_id: int = 0, progress_object: object = None):
+    completed = 0
+    total = len(links)
+    download_dir += '/' + query
+    # By default, the executor sets number of workers to 5 times the number of
+    # CPUs.
+    with ThreadPoolExecutor() as executor:
+        # Iterate through links and schedule image download instead of mapping
+        # in order to track progress
+        future_to_url = {executor.submit(download_image, download_dir, link): link['url'] for link in links}
+        for future in concurrent.futures.as_completed(future_to_url):
+            completed += 1
+            url = future_to_url[future]
+            _, status = future.result()
+            if progress_object:
+                progress_object.updateProgress.emit(query_id, (completed / total) * 100)
+    # In case the signal was missed (bug?)
+    if progress_object:
+        progress_object.updateProgress.emit(query_id, 100)
