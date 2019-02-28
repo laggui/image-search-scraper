9c5b5e3b32935d078a04f245841a499d623674b1
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2019-02-27 20:14:08 -0500

    Added signal on icon click

--- widgets/plus_icon.py
+++ widgets/plus_icon.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QHBoxLayout, QLabel, QWidget
+from PyQt5.QtCore import pyqtSignal
 
 from widgets import ImageButton
 
@@ -6,16 +7,16 @@ class PlusIcon(QWidget):
     """
     Plus icon widget with specified description text to its right
     """
+    add = pyqtSignal()
     def __init__(self, text: str, size: int = 24, parent: QWidget = None):
         super().__init__(parent)
         # Layout
         hLayout = QHBoxLayout()
-        self.plusIcon = ImageButton('plus', size, size)
-        hLayout.addWidget(self.plusIcon)
+        plusIcon = ImageButton('plus', size, size)
+        hLayout.addWidget(plusIcon)
         hLayout.addWidget(QLabel(text))
         hLayout.addStretch(1)
         self.setLayout(hLayout)
 
-    def clickableIcon(self):
-        """Returns the clickable icon in order to access its clicked signal externally"""
-        return self.plusIcon
+        # Connect button signal
+        plusIcon.clicked.connect(self.add)
