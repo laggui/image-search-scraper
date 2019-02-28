bf3666de2a22bc1b223e724dc22845e5ecad9c3c
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2019-02-27 20:19:39 -0500

    Added toolbar, progress dock and google search task

--- image_search_scraper.py
+++ image_search_scraper.py
@@ -5,15 +5,16 @@ from pathlib import Path
 from PyQt5.QtWidgets import QApplication, QMainWindow, QScrollArea
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QMessageBox
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QFrame, QAction, QFileDialog
-from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, pyqtSlot
 
-from widgets import ImageButton, PlusIcon, SearchClient, ToolBar
-from widgets.search_client import SupportedSearchClients
+from widgets import ImageButton, PlusIcon, ProgressDock, SearchClient, SearchTask, ToolBar
 from widgets.utils import newIcon
 
-__appname__ = 'Dataset Builder'
+from webscraper import SupportedSearchClients
 
-class DatasetBuilderApp(QMainWindow):
+__appname__ = 'Image Search Scraper'
+
+class ImageSearchScraperApp(QMainWindow):
     searchCountUpdated = pyqtSignal()
     clientCountUpdated = pyqtSignal()
     def __init__(self):
@@ -21,9 +22,9 @@ class DatasetBuilderApp(QMainWindow):
         self.title = __appname__
         self.left = 100
         self.top = 100
-        self.width = 800
-        self.height = 640
-        self.defaultSaveDir = str(Path.home())
+        self.width = 1024
+        self.height = 768
+        self.defaultSaveDir = str(Path.home()).replace('\\', '/')
         self.searchCount = 0
         self.clientCount = 0
         self.initUI()
@@ -66,6 +67,8 @@ class DatasetBuilderApp(QMainWindow):
         scroll.setWidgetResizable(True)
         scroll.setFrameShape(QFrame.NoFrame)
         scroll.setContentsMargins(0, 0, 0, 0)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        scroll.setMinimumWidth(250)
         scrollLayout = QVBoxLayout()
         scrollLayout.addWidget(scroll)
         scrollAreaContent.setLayout(listLayout)
@@ -77,6 +80,8 @@ class DatasetBuilderApp(QMainWindow):
         self.addToolBar(Qt.TopToolBarArea, self.toolbar)
 
         # Dock widgets
+        self.downloadProgressDock = ProgressDock('Queries Download Progress')
+        self.addDockWidget(Qt.RightDockWidgetArea, self.downloadProgressDock)
 
         # Connect signals and slots
         addGoogleScraper.clicked.connect(lambda state, x=SupportedSearchClients.GOOGLE: self.addSearchClient(x))
@@ -84,10 +89,11 @@ class DatasetBuilderApp(QMainWindow):
         self.clientCountUpdated.connect(self.updateToolbar)
         addGoogleAPI.clicked.connect(lambda state, x=SupportedSearchClients.GOOGLE_API: self.addSearchClient(x))
         addBingAPI.clicked.connect(lambda state, x=SupportedSearchClients.BING_API: self.addSearchClient(x))
-        self.toolbar.setSaveDirButton().clicked.connect(self.setDefaultSaveDirectory)
-        # self.toolbar.searchAllButton().clicked.connect(self.scrapeAllImages)
-        self.toolbar.deleteAllButton().clicked.connect(self.removeAllSearchClients)
+        self.toolbar.setDefaultDirectory.connect(self.setDefaultSaveDirectory)
+        # self.toolbar.searchAll.connect(self.searchAllQueries)
+        self.toolbar.deleteAll.connect(self.removeAllSearchClients)
         firstSearchClient.delete.connect(self.updateClientCount)
+        firstSearchClient.search[str, str, str, str, int].connect(self.startGoogleAPISearchTask)
         firstSearchClient.searchCountUpdated[int, str].connect(self.updateSearchCount)
 
         # Window settings
@@ -97,10 +103,41 @@ class DatasetBuilderApp(QMainWindow):
         self.center()
         self.show()
 
+    @pyqtSlot(str, str, int)
+    def startGoogleSearchTask(self, directory: str, query: str, numImages: int):
+        """
+        Start Google Image Scraper search task
+        """
+        directory = directory + '/Google_API'
+        self.downloadProgressDock.addProgressItem(directory + '/' + query)
+        idx = self.downloadProgressDock.getItemCount() - 1
+        self.thread = QThread(self)
+        self.searchTask = SearchTask(idx, SupportedSearchClients.GOOGLE, directory, query, numImages)
+        self.searchTask.moveToThread(self.thread)
+        self.searchTask.finished.connect(self.thread.terminate)
+        self.searchTask.updateProgress[int, float].connect(self.downloadProgressDock.setProgressItemValue)
+        self.thread.started.connect(self.searchTask.executeSearchTask)
+        self.thread.start()
+        
+
+    @pyqtSlot(str, str, str, int)
+    def startBingAPISearchTask(self, apiKey: str, directory: str, query: str, numImages: int):
+        """
+        Start Bing Image Search API search task
+        """
+        print('Start Bing Image Search API search task')
+
+    @pyqtSlot(str, str, str, str, int)
+    def startGoogleAPISearchTask(self, apiKey: str, cseID: str, directory: str, query: str, numImages: int):
+        """
+        Start Google Custom Search JSON API search task
+        """
+        print('Start Google Custom Search JSON API search task')
+
     @pyqtSlot()
     def updateToolbar(self):
-        self.toolbar.searchAllButton().setEnabled(self.searchCount > 0)
-        self.toolbar.deleteAllButton().setEnabled(self.clientCount > 0)
+        self.toolbar.setSearchAllEnabled(self.searchCount > 0)
+        self.toolbar.setDeleteAllEnabled(self.clientCount > 0)
 
     @pyqtSlot()
     def setDefaultSaveDirectory(self):
@@ -115,7 +152,7 @@ class DatasetBuilderApp(QMainWindow):
     @pyqtSlot()
     def removeAllSearchClients(self):
         mboxtitle = 'Delete All'
-        mboxmsg = 'Are you sure you want to delete all search API instances and their corresponding queries?'
+        mboxmsg = 'Are you sure you want to delete all search client instances and their corresponding queries?'
         reply = QMessageBox.warning(self, mboxtitle, mboxmsg,
                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
@@ -126,10 +163,16 @@ class DatasetBuilderApp(QMainWindow):
     def addSearchClient(self, client: SupportedSearchClients):
         self.clientCount += 1
         self.searchCount += 1
-        client = SearchClient(client, self.defaultSaveDir)
-        self.clientsLayout.addWidget(client)
-        client.delete.connect(self.updateClientCount)
-        client.searchCountUpdated[int, str].connect(self.updateSearchCount)
+        clientWidget = SearchClient(client, self.defaultSaveDir)
+        self.clientsLayout.addWidget(clientWidget)
+        clientWidget.delete.connect(self.updateClientCount)
+        clientWidget.searchCountUpdated[int, str].connect(self.updateSearchCount)
+        if client == SupportedSearchClients.GOOGLE_API:
+            clientWidget.search[str, str, str, str, int].connect(self.startGoogleAPISearchTask)
+        elif client == SupportedSearchClients.BING_API:
+            clientWidget.search[str, str, str, int].connect(self.startBingAPISearchTask)
+        elif client == SupportedSearchClients.GOOGLE:
+            clientWidget.search[str, str, int].connect(self.startGoogleSearchTask)
         self.clientCountUpdated.emit()
 
     @pyqtSlot()
@@ -166,5 +209,5 @@ class DatasetBuilderApp(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = DatasetBuilderApp()
+    w = ImageSearchScraperApp()
     sys.exit(app.exec_())
